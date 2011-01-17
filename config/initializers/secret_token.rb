e1389f5e23c520e3906ed5210ef467dfb794c5a7
Your Name <sly.thu@gmail.com>
2011-01-17 17:37:42 +0800

    add initial project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XPlan::Application.config.secret_token = '781efa2ac58d2a07cf5e179e7f4b4798e030523d7523fcc0a24d9fb82bfe8e4e26a068cb985b2497088a9917fb51deaf9e1c4daa75cfae4c34155e014b58b6cc'
